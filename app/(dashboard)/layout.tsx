ac3e702765ecac8061607b9ed0c87db3e045e912
ozymandias54 <bassirou718@gmail.com>
2025-08-18 13:50:49 +0000

    sidebar init

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,8 @@
 import { getAuth } from "@/lib/isAuth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar"
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
@@ -10,11 +12,24 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     getAuth().then((data) => {
       if (data === null) {
         router.push("/login");
+      } else {
+        console.log("Utilisateur connecté :", data.payload);
       }
-      console.log("Utilisateur connecté :", data?.payload);
     });
   }, []);
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)"
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>{children}</SidebarInset>
+    </SidebarProvider>
+  );
 };
 
 export default DashboardLayout;
---
+++ components/sidebar/app-sidebar.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import * as React from "react";
+import {
+  IconChartBar,
+  IconDashboard,
+  IconFolder,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconUsers
+} from "@tabler/icons-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar";
+import { NavMain } from "./nav-main";
+import { NavUser } from "./nav-user";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg"
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard
+    },
+    {
+      title: "Lifecycle",
+      url: "#",
+      icon: IconListDetails
+    },
+    {
+      title: "Analytics",
+      url: "#",
+      icon: IconChartBar
+    },
+    {
+      title: "Projects",
+      url: "#",
+      icon: IconFolder
+    },
+    {
+      title: "Team",
+      url: "#",
+      icon: IconUsers
+    }
+  ]
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />{" "}
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
