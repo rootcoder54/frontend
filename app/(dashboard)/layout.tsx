70ea645042a80fe95931352da2b8f015d1b60cb1
ozymandias54 <bassirou718@gmail.com>
2025-09-17 14:32:46 +0000

    20250917_1432

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { QueryProvider } from "@/components/provider/query-provider";
 import { useAuth } from "@/hooks/useAuth";
@@ -27,6 +28,10 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
     );
   }
+  if (!user) {
+    // On retourne rien, car le router va rediriger
+    return null;
+  }
   return (
     <QueryProvider>
       <SidebarProvider
@@ -38,7 +43,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         }
       >
         <AppSidebar variant="inset" />
-        <SidebarInset>{children}</SidebarInset>
+        <SidebarInset>
+          <ScrollArea className="rounded-md h-[calc(100vh-40px)]">
+            {children}
+          </ScrollArea>
+        </SidebarInset>
       </SidebarProvider>
     </QueryProvider>
   );
